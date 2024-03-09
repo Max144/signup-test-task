e36f236df025becadeef976bd27e53ea705d77e9
Maxim Fadeev <m.fadeev@geniusee.com>
2024-03-09 23:31:43 +0200

    add inforu config variables

---
+++ config/external-apis.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'inforu' => [
+        'xml_endpoint' => env('INFORU_XML_ENDPOINT'),
+        'login' => env('INFORU_LOGIN'),
+        'api_token' => env('INFORU_API_TOKEN'),
+        'sender_name' => env('INFORU_SENDER_NAME'),
+    ]
+];
